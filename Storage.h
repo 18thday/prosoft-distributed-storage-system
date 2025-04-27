117791a0f27d08df2328c8b25f62b4d4f18ac5c4
RexPraia <118528233+RexPraia@users.noreply.github.com>
2025-04-27 22:41:46 +0500

    Update Storage.h

--- Storage.h
+++ Storage.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <boost/filesystem.hpp>
 
 struct FilePart {
    // char* firstBytes;
@@ -11,11 +12,21 @@ struct FilePart {
     std::string& hashFilePath;
     std::string& hashFilePartPath;
 };
+/*
+    Эту структуру я создал для реализации метода объединения частей файла, этот момент надо будет обсудить.
+    Использовал вектор vector<filePart2> fileParts первая ячейка которого занята информацие о файле (изначальное название файла)
+    В остальных ячейках будет содержаться информация о частях файла (имя и путь) 
 
+*/
+struct filePart2 {
+    std::string fileName;
+    std::string partFileName;
+    std::string partFilePath;
+};
 class Storage
 {
 public:
-    Storage(const std::string& filePath, const int numberOfParts); // , size_t partSizeInBytes)
+    //Storage(/*const std::string& filePath, const int numberOfParts*/) {} // , size_t partSizeInBytes)
     
     /**
       * @brief Деление файла на части
@@ -29,7 +40,7 @@ public:
       * @param std::vector<FilePart> Вектор со всей информация о частях
       * @patam string& outputPath Путь для скачивания объединенного файла
     */
-    static void mergeFile(const std::vector<FilePart>, const std::string& outputPath);
+    static void mergeFile(const std::vector<filePart2> fileParts, const std::string& outputPath);
     /**
       * @brief Перераспределение частей файлов
       * @param std::vector<FilePart> Вектор со всей информация о имеющихся частях файла старой конфигурации
@@ -40,7 +51,21 @@ public:
 
        
 private:
-std::string& filePath; // Путь файла
-int numberOfParts; // Число частей на которые нужно разделить фаил
+    /**
+     * @brief Проверяет, достаточно ли места на диске для разделения файла.
+     * @param fileSize Размер файла в байтах.
+     * @return True, если на диске достаточно места; иначе False.
+     */
+    bool hasEnoughSpace(size_t fileSize, std::string tempDir) const;
+    /**
+     * @brief Очищает имя файла от недопустимых символов (например, пробелов).
+     * @param filename Имя файла для очистки.
+     * @return Очищенное имя файла.
+     */
+    std::string sanitizeFilename(const std::string& filename) const; // Обработка пробелов и спецсимволов
+
+    std::string& filePath; // Путь файла
+    int numberOfParts; // Число частей на которые нужно разделить фаил
+    
 }
 
