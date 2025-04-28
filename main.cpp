5232b5a2a2080f5de10cfb616c7efe2dcf645c36
Blacktopus <viktor8lysenko@gmail.com>
2025-04-28 21:49:31 +0500

    update storage

--- main.cpp
+++ main.cpp
@@ -1,6 +1,75 @@
-#include <iostream>
+/*#include <iostream>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
-}
+}*/
+// для проверки работы
+ //main.cpp
+
+ // #include <iostream>
+ // #include "src/Storage/filesplitter.h"
+ // #include "src/Storage/hashsummmanager.h"
+ // #include <string>
+ // #include <boost/filesystem.hpp>
+
+
+ // int main() {
+ //     FileSplitter splitter;
+ //     std::string filePath = "/home/qwuser/Downloads/Periodic cooking of eggs.pdf"; // Замените на реальный путь к файлу
+ //     boost::filesystem::path tempDir = boost::filesystem::temp_directory_path();
+ //     std::string tempDirStr = tempDir.string();
+ //   //      = "/tmp/temp_dir";      // Замените на реальную временную директорию
+ //     size_t chunkSize = 4096;
+
+ //     std::cout << "temp dir " + tempDirStr << std::endl;
+ //     try {
+ //         splitter.split(filePath, tempDirStr, chunkSize);
+ //         std::cout << "Файл успешно разделен." << std::endl;
+ //     } catch (const std::runtime_error& error) {
+ //         std::cerr << "Ошибка при разделении файла: " << error.what() << std::endl;
+ //         return 1; // Возвращаем код ошибки
+ //     }
+
+ //     return 0;
+ // }
+
+#include "src/Storage/hashsummmanager.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+ int main() {
+     HashSummManager manager;
+
+     // 1. Проверка calculateFileHash
+     std::string filePath = "/home/qwuser/Downloads/Periodic cooking of eggs.pdf"; // Создайте этот файл для теста
+     // std::ofstream outFile(filePath);
+     // outFile << "This is a test file.";
+     // outFile.close();
+
+     size_t fileHash1 = manager.calculateFileHash(filePath);
+     std::cout << "Хэш файла '" << filePath << "': " << fileHash1 << std::endl;
+     size_t fileHash2 = manager.calculateFileHash(filePath);
+     std::cout << "Хэш файла '" << filePath << "': " << fileHash2 << std::endl;
+
+     // 2. Проверка compareHashes
+     size_t hash1 = fileHash1;
+     size_t hash2 = fileHash2;
+     size_t hash3 = 12345;
+
+     std::cout << "Сравнение хешей (" << hash1 << ") и (" << hash2 << "): " << (manager.compareHashes(hash1, hash2) ? "Истина" : "Ложь") << std::endl;
+     std::cout << "Сравнение хешей (" << hash1 << ") и (" << hash3 << "): " << (manager.compareHashes(hash1, hash3) ? "Истина" : "Ложь") << std::endl;
+
+     // 3. Проверка calculateDirectoryHash
+     std::string directoryPath = "/tmp/Periodic-cooking-of-eggs"; // Текущая директория
+     std::string filenameContains = "Periodic-cooking-of-eggs";  // Ищем файлы, содержащие "test" в имени
+
+     size_t directoryHash = manager.calculateDirectoryHash(directoryPath);//, filenameContains);
+     std::cout << "Хэш директории '" << directoryPath << "' : " << directoryHash << std::endl;
+
+     // Очистка тестового файла
+     //std::remove(filePath.c_str());
+
+     return 0;
+ }
---
+++ src/Storage/storage.h
@@ -0,0 +1,76 @@
+#ifndef STORAGE_H
+#define STORAGE_H
+
+#pragma once
+#include <vector>
+#include <string>
+#include <boost/filesystem.hpp>
+
+struct FilePart {
+    // char* firstBytes;
+    size_t partSize;
+    size_t partNumber;
+    unsigned long filePratHash;
+    unsigned long fileHash;
+    std::string& hashFilePath;
+    std::string& hashFilePartPath;
+};
+/*
+    Эту структуру я создал для реализации метода объединения частей файла, этот момент надо будет обсудить.
+    Использовал вектор vector<filePart2> fileParts первая ячейка которого занята информацие о файле (изначальное название файла)
+    В остальных ячейках будет содержаться информация о частях файла (имя и путь)
+
+*/
+struct filePart2 {
+    std::string fileName;
+    std::string partFileName;
+    std::string partFilePath;
+};
+class Storage
+{
+public:
+    Storage();
+    //Storage(/*const std::string& filePath, const int numberOfParts*/) {} // , size_t partSizeInBytes)
+
+    /**
+      * @brief Деление файла на части
+      * @param filePath Путь к файлу
+      * @param numberOfParts Число частей
+      * @return std::vector<FilePart> Возвращаем вектор структур с заполненными данными деления
+    */
+    static std::vector<FilePart> splitFile(const std::string& filePath, const int numberOfParts);
+    /**
+      * @brief Объединение кусков файла в единый
+      * @param std::vector<FilePart> Вектор со всей информация о частях
+      * @patam string& outputPath Путь для скачивания объединенного файла
+    */
+    static void mergeFile(const std::vector<filePart2> fileParts, const std::string& tempDir, const std::string& outputPath);
+    /**
+      * @brief Перераспределение частей файлов
+      * @param std::vector<FilePart> Вектор со всей информация о имеющихся частях файла старой конфигурации
+      * @patam int newNumbersOfParts Новое колличество частей(узлов)
+      * @return vector<FilePart> Возвращаем вектор с актуальными разделениями
+    */
+    static std::vector<FilePart> reconfiguration(const std::vector<FilePart>, const int newNumbersOfParts);
+
+
+private:
+    /**
+     * @brief Проверяет, достаточно ли места на диске для разделения файла.
+     * @param fileSize Размер файла в байтах.
+     * @return True, если на диске достаточно места; иначе False.
+     */
+    static bool hasEnoughSpace(size_t fileSize, std::string tempDir);
+    /**
+     * @brief Очищает имя файла от недопустимых символов (например, пробелов).
+     * @param filename Имя файла для очистки.
+     * @return Очищенное имя файла.
+     */
+    std::string sanitizeFilename(const std::string& filename) const; // Обработка пробелов и спецсимволов
+
+    std::string& filePath; // Путь файла
+    int numberOfParts; // Число частей на которые нужно разделить фаил
+
+};
+
+#endif // STORAGE_H
