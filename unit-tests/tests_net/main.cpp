21bab425ea7bdbcb63a2c826fe1768ae6b694cb9
Sergey-A-98 <serjmercurial@yandex.ru>
2025-04-30 02:00:01 +0500

    заготовка для unit-тестов

---
+++ unit-tests/tests_net/main.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MODULE My test module
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE( Tests )
+
+BOOST_AUTO_TEST_CASE( Tests )
+{
+    BOOST_TEST( true /* test assertion */ );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
