5f71d5eb0b186d46f3656b131a0a7d5b9e7013a4
Sergey-A-98 <serjmercurial@yandex.ru>
2025-04-30 02:07:22 +0500

    unit-тесты сетевого взаимодействия

---
+++ unit-tests/tests_net/build/Desktop-Debug/CMakeFiles/tests_net.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests_net.dir/main.cpp.o"
+  "CMakeFiles/tests_net.dir/main.cpp.o.d"
+  "tests_net"
+  "tests_net.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests_net.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
