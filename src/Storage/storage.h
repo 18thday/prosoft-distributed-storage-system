d19466daff8415ef82570305f3a22c64dc21c93a
Viktor Lysenko <122009597+Blacktopus@users.noreply.github.com>
2025-04-29 10:21:26 +0500

    Update storage.h, add doxygen comments

--- src/Storage/storage.h
+++ src/Storage/storage.h
@@ -11,18 +11,22 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 
-
+/**
+ * @class Storage
+ * @brief Класс для работы с файлами, будетиспользоваться и клиентом и узлами.
+ */
 class Storage
 {
 public:
     //Storage();
     //Storage(/*const std::string& filePath, const int numberOfParts*/) {} // , size_t partSizeInBytes)
     /**
-      * @brief Деление файла на части
-      * @param filePath Путь к файлу
-      * @param numberOfParts Число частей
-      * @return std::vector<FilePart> Возвращаем вектор структур с заполненными данными деления
-    */
+     * @brief Делит файл на части заданного размера.
+     * @param filePath Путь к исходному файлу.
+     * @param tempDir Директория для временного хранения частей.
+     * @param chunkSize Размер каждой части в байтах.
+     * @return std::string Путь к директории с частями файлов.
+     */
     static std::string splitFile(const std::string& filePath,
                                  const std::string& tempDir,
                                  const size_t chunkSize);
@@ -48,28 +52,47 @@ public:
       * @patam int newNumbersOfParts Новое колличество частей(узлов)
       * @return vector<FilePart> Возвращаем вектор с актуальными разделениями
     */
+    
 
-    /// идет в директорию, читает каждый файл и преобразует его содержимое в строку
-    /// наверное проще вернуть вектор, но не любой файл засунешь в оперативку
-    static std::string uploadData(const std::string& splitFileDir);
-    /// получает строки создает из них json
+    
+    /**
+     * @brief Загружает данные из файлов в указанной директории.
+     * @param splitFileDir Директория с частями файлов.
+     * @return std::string Содержимое каждого отдельного файла в виде строки.
+     */
+    static std::string uploadData(const std::string& splitFileDir);/// наверное проще вернуть вектор, но не любой файл засунешь в оперативку
+    /**
+     * @brief Создает JSON из строковых данных.
+     * @param tempDir Директория для временного хранения данных.
+     * @param data Строка с данными для преобразования в JSON.
+     * @return std::string JSON-строка.
+     */
     static std::string reciveData(const std::string& tempDir,
                                   const std::string& data);
 
 private:
     /**
-     * @brief Проверяет, достаточно ли места на диске для разделения файла.
+     * @brief Проверяет, достаточно ли места на диске для разделения файла. Сейчас не используется.
      * @param fileSize Размер файла в байтах.
      * @return True, если на диске достаточно места; иначе False.
      */
     static bool hasEnoughSpace(size_t fileSize, std::string tempDir);
     /**
-     * @brief Очищает имя файла от недопустимых символов (например, пробелов).
+     * @brief Очищает имя файла от недопустимых символов (например, пробелов). Сейчас не используется.
      * @param filename Имя файла для очистки.
      * @return Очищенное имя файла.
      */
     static std::string sanitizeFilename(const std::string& filename); // Обработка пробелов и спецсимволов
-    ///
+    /**
+     * @brief Создает JSON с информацией о файле.
+     * @param file_name Путь к файлу.
+     * @param file_size Размер файла.
+     * @param file_hash Хэш файла.
+     * @param file_type Тип файла.
+     * @param chunk_size Размер части.
+     * @param chunk_count Количество частей.
+     * @param outputDir Директория для сохранения JSON.
+     */
     static void createFileInfoJson(const boost::filesystem::path& file_name,
                                    size_t file_size,
                                    size_t file_hash,
@@ -77,7 +100,15 @@ private:
                                    size_t chunk_size,
                                    size_t chunk_count,
                                    const boost::filesystem::path& outputDir);
-    ///
+    /**
+     * @brief Создает JSON для отдельной части файла, с именем: filename_chunkX.part.
+     * @param file_name Имя исходного файла.
+     * @param chunk_name Имя части файла.
+     * @param chunk_number Номер части.
+     * @param chunk_size Размер части.
+     * @param data Строка с бинарными данными в формате base64 части файла.
+     * @param output_file Путь для сохранения JSON.
+     */
     static void createChunkJson(const std::string& file_name,
                                 const std::string& chunk_name,
                                 int chunk_number,
@@ -85,15 +116,32 @@ private:
                                 const std::string& data,
                                 const boost::filesystem::path& output_file);
 
-    /// пока не придумал этому применение
+    /**
+     * @brief Изменяет JSON файл (функция в разработке).
+     */
     static void changeJsonFile();
-    /// создает JSON со списком файлов
+    /**
+     * @brief Создает JSON со списком файлов в директории.
+     */
     static void createFilesListJson();
-    /// чистит директорию
+    /**
+     * @brief Очищает указанную директорию, dirPath тоже удаляется.
+     * @param dirPath Путь к директории для очистки.
+     */
     static void clearDirectory(const boost::filesystem::path& dirPath);
-    /// может пригодится при сборке файла или тоже вычислить его в merge
-    static size_t calculateFileHash(const boost::filesystem::path& filePath, const size_t& chunkSize);
-    /// преобразует бинарные данные в строку
+    /**
+     * @brief Вычисляет хэш файла.
+     * @param filePath Путь к файлу.
+     * @param chunkSize Размер части.
+     * @return size_t Хэш файла.
+     */
+    static size_t calculateFileHash(const boost::filesystem::path& filePath, const size_t& chunkSize);/// может пригодится при сборке файла или тоже вычислить его в merge
+    /**
+     * @brief Кодирует бинарные данные в строку в формате base64.
+     * @param data Указатель на данные.
+     * @param size Размер данных.
+     * @return std::string Закодированные данные в формате base64.
+     */
     static std::string base64_encode(const char* data, size_t size);
 };
 
