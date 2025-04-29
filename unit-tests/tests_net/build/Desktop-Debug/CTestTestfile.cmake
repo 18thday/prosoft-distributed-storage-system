53c0b757bd758f6bdfa18742b9238be41659b8b8
Sergey-A-98 <serjmercurial@yandex.ru>
2025-04-30 02:12:54 +0500

    Удалил лишние файлы

--- unit-tests/tests_net/build/Desktop-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sami/coding/test_pdss/tests_net
-# Build directory: /home/sami/coding/test_pdss/tests_net/build/Desktop-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tests_net "/home/sami/coding/test_pdss/tests_net/build/Desktop-Debug/tests_net")
-set_tests_properties(tests_net PROPERTIES  _BACKTRACE_TRIPLES "/home/sami/coding/test_pdss/tests_net/CMakeLists.txt;11;add_test;/home/sami/coding/test_pdss/tests_net/CMakeLists.txt;0;")
